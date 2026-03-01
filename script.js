8a2c4f6aba484117c4874ad6bb00d0a013017811
maneki-kakutasu <184769383+maneki-kakutasu@users.noreply.github.com>
2026-03-01 12:14:24 +1000

    The rest of the game, I guess...
    Modified getComputerChoice function to return string values
    Wrote getPlayerChoice function to get player's choice
    Wrote playRound and playGame functions to play multiple rounds

--- script.js
+++ script.js
@@ -1,13 +1,64 @@
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3);
-
-    if(choice === 0) {
+    let computerChoice = Math.floor(Math.random() * 3);
+    
+    if(computerChoice === 0) {
         console.log("rock");
-    } else if(choice === 1) {
+        return "rock";
+    } else if(computerChoice === 1) {
         console.log("paper");
+        return "paper";
     } else {
         console.log("scissors");
+        return "scissors";
     }
 }
 
-// function getHumanChoice() {}
+function getPlayerChoice() {
+    let playerChoice = prompt("Rock, paper, scissors?");
+    console.log(playerChoice);
+    return playerChoice;
+}
+
+playerScore = 0;
+computerScore = 0;
+
+function playRound(playerChoice, computerChoice) {
+    if(playerChoice === "rock" && computerChoice === "scissors") {
+        playerScore += 1;
+        console.log("YOU WIN!");
+        console.log("Your score: ", playerScore);
+        console.log("Computer score: ", computerScore);
+    } else if(playerChoice === "paper" && computerChoice === "rock") {
+        playerScore += 1;
+        console.log("YOU WIN!");
+        console.log("Your score: ", playerScore);
+        console.log("Computer score: ", computerScore);
+    } else if(playerChoice === "scissors" && computerChoice === "paper") {
+        playerScore += 1;
+        console.log("YOU WIN!")
+        console.log("Your score: ", playerScore);
+        console.log("Computer score: ", computerScore);
+    } else if(playerChoice === computerChoice) {
+        console.log("DRAW!");
+        console.log("Your score: ", playerScore);
+        console.log("Computer score: ", computerScore);
+    } else {
+        computerScore += 1;
+        console.log("YOU LOSE!");
+        console.log("Your score: ", playerScore);
+        console.log("Computer score: ", computerScore);
+    }
+}
+
+function playGame() {
+    let rounds = 1;
+
+    while(rounds <= 5) {
+        playerChoice = getPlayerChoice();
+        computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice);
+        rounds += 1;
+    }
+}
+
+playGame();
